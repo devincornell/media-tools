1de0c44d4de3bee00fc6c42a02c5f45898ab6c6c
Devin Cornell <devinj.cornell@gmail.com>
2025-08-16 09:40:45 -0400

    adding functionality to VideoFile for copying/moving/deletign and calling ffmpeg commands with this single video as input.

--- src/mediatools/video/video_file.py
+++ src/mediatools/video/video_file.py
@@ -6,14 +6,14 @@ import typing
 #import pathlib
 from pathlib import Path
 import tqdm
-
+import shutil
 import datetime
-
+import hashlib
 import mediatools.util
 
 from .video_info import VideoInfo
 from .errors import VideoFileDoesNotExistError
-from .ffmpeg import FFMPEG, FFMPEGResult, ProbeInfo, NoDurationError, probe
+from .ffmpeg import FFMPEG, FFMPEGResult, ProbeInfo, NoDurationError, probe, LOGLEVEL_OPTIONS
 
 
 
@@ -46,27 +46,99 @@ class VideoFile:
         return self.fpath.exists()
 
     ############################# Utility #############################
-    def is_probable(self) -> bool:
-        '''Returns True of video file is probe-able.'''
-        try:
-            self.probe()
-        except Exception as e:
-            return False
-        else:
-            return True
-
-    def probe(self, check_for_errors: bool = False) -> ProbeInfo:
+    def probe(self) -> ProbeInfo:
         '''Probe the file in question.'''
         return probe(str(self.fpath))
+
+    def hash(filepath: Path|str, hash_func=hashlib.sha256):
+        """Calculate file hash with optimal chunk size."""
+        h = hash_func()
+        chunk_size = 128 * h.block_size  # Optimal chunk size
+    
+        with open(filepath, 'rb') as f:
+            for chunk in iter(lambda: f.read(chunk_size), b''):
+                h.update(chunk)
+        return h.hexdigest()
+    
+    def size(self) -> int:
+        '''Get the size of the video file in bytes.'''
+        return self.fpath.stat().st_size
 
     ############################# file operations #############################
     def copy(self, new_fpath: Path, overwrite: bool = False) -> VideoFile:
         '''Copy the file to a new location.'''
         new_fpath = Path(new_fpath)
         if new_fpath.exists() and not overwrite:
-            raise FileExistsError(f'The file {new_fpath} already exists. User overwrite=True to overwrite it.')
-        new_fpath.write_bytes(self.fpath.read_bytes())
+            raise FileExistsError(f'The file "{new_fpath}" already exists. ')
+        shutil.copy2(self.fpath, new_fpath)
         return VideoFile.from_path(new_fpath)
+    
+    def move(self, new_fpath: Path, overwrite: bool = False) -> VideoFile:
+        '''Copy the file to a new location.'''
+        if overwrite:
+            self.fpath.replace(target=new_fpath)
+        else:
+            self.fpath.rename(target=new_fpath)
+        return VideoFile.from_path(new_fpath)
+
+
+    ############################# file operations #############################
+    def ffmpeg(
+        self,
+        output_file: str|Path,
+        overwrite_output: bool = False,
+        ss: str|None = None,
+        duration: str|None = None,
+        vf: str|None = None,
+        af: str|None = None,
+        vcodec: str|None = None,
+        acodec: str|None = None,
+        video_bitrate: str|None = None,
+        audio_bitrate: str|None = None,
+        framerate: int|None = None,
+        format: str|None = None,
+        filter_complex: str|None = None,
+        disable_audio: bool = False,
+        disable_video: bool = False,
+        crf: int|None = None,
+        preset: str|None = None,
+        hwaccel: str|None = None,
+        loglevel: LOGLEVEL_OPTIONS|None = None,
+        hide_banner: bool = True,
+        nostats: bool = True,
+        output_args: list[tuple[str,str]]|None = None,
+        input_args: list[tuple[str,str]]|None = None,
+        command_flags: list[str]|None = None,
+    ) -> FFMPEGResult:
+        '''Execute an ffmpeg command.'''
+        return FFMPEG(
+            input_files = [self.fpath],
+            output_file = output_file,
+            overwrite_output = overwrite_output,
+            ss = ss,
+            duration = duration,
+            vf = vf,
+            af = af,
+            vcodec = vcodec,
+            acodec = acodec,
+            video_bitrate = video_bitrate,
+            audio_bitrate = audio_bitrate,
+            framerate = framerate,
+            format = format,
+            filter_complex = filter_complex,
+            disable_audio = disable_audio,
+            disable_video = disable_video,
+            crf = crf,
+            preset = preset,
+            hwaccel = hwaccel,
+            loglevel = loglevel,
+            hide_banner = hide_banner,
+            nostats = nostats,
+            output_args = output_args,
+            input_args = input_args,
+            command_flags = command_flags,
+        ).run()
+
 
 
 @dataclasses.dataclass
