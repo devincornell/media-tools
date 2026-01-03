48352bec4d64193fc2d22fe0a9c3339fec5baa21
Devin Cornell <devinj.cornell@gmail.com>
2026-01-02 20:17:00 -0500

    created function alias get_hash_firstmb_hex

--- scripts/util.py
+++ scripts/util.py
@@ -13,6 +13,10 @@ def get_hash_hex_THUMB(vid_path_abs: Path) -> str:
     '''Creates a hash from first 1000 kb chunks. Used for thumbnails filenames.'''
     return get_hash_hex(vid_path_abs, chunk_size=1024, max_chunks=1000)
 
+def get_hash_firstmb_hex(vid_path_abs: Path) -> str:
+    '''Creates a hash from first 1000 kb chunks. Used for thumbnails filenames.'''
+    return get_hash_hex(vid_path_abs, chunk_size=1024, max_chunks=1000)
+
 def get_hash_hex(file_path: Path, chunk_size: int = 1024, max_chunks: int|None = None) -> str:
     '''Creates a SHA256 hash from the file. Only uses up to max_chunks of chunk_size bytes.'''
     sha256_hash = hashlib.sha256()
